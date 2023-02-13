2e601178ed632ebf293f24b72e6abe4ed94ea16c
AshimbaevDamir <122977007+AshimbaevDamir@users.noreply.github.com>
2023-02-13 14:46:48 +0300

    Add files via upload

---
+++ logo/fox.js
@@ -0,0 +1,5 @@
+const fox = document.querySelector('.fox');
+
+fox.addEventListener('click', () => {
+    location.reload()
+});
---
+++ slider/app.js
@@ -0,0 +1,200 @@
+document.querySelector('.sInfo').addEventListener('click', () => {
+  if (document.querySelector('.me2').classList.contains('active')){
+    document.querySelector('.me2').classList.add('remove');
+  };
+  if (document.querySelector('.me3').classList.contains('active')){
+    document.querySelector('.me3').classList.add('remove');
+  };
+  if (document.querySelector('.me4').classList.contains('active')){
+    document.querySelector('.me4').classList.add('remove');
+  };
+  if (document.querySelector('.me5').classList.contains('active')){
+    document.querySelector('.me5').classList.add('remove');
+  };
+
+
+  if (document.querySelector('.me2').classList.contains('active')){
+    document.querySelector('.me2').classList.remove('active');
+  };
+  if (document.querySelector('.me3').classList.contains('active')){
+    document.querySelector('.me3').classList.remove('active');
+  };
+  if (document.querySelector('.me4').classList.contains('active')){
+    document.querySelector('.me4').classList.remove('active');
+  };
+  if (document.querySelector('.me5').classList.contains('active')){
+    document.querySelector('.me5').classList.remove('active');
+  };
+
+  
+  if (document.querySelector('.me1').classList.contains('remove')){
+    document.querySelector('.me1').classList.remove('remove');
+    
+  };
+
+  document.querySelector('.me1').classList.add('active');
+  
+  
+}); 
+
+document.querySelector('.edInfo').addEventListener('click', () => {
+  if (document.querySelector('.me1').classList.contains('active')){
+    document.querySelector('.me1').classList.add('remove');
+  };
+  if (document.querySelector('.me3').classList.contains('active')){
+    document.querySelector('.me3').classList.add('remove');
+  };
+  if (document.querySelector('.me4').classList.contains('active')){
+    document.querySelector('.me4').classList.add('remove');
+  };
+  if (document.querySelector('.me5').classList.contains('active')){
+    document.querySelector('.me5').classList.add('remove');
+  };
+
+
+  if (document.querySelector('.me1').classList.contains('active')){
+    document.querySelector('.me1').classList.remove('active');
+  };
+  if (document.querySelector('.me3').classList.contains('active')){
+    document.querySelector('.me3').classList.remove('active');
+  };
+  if (document.querySelector('.me4').classList.contains('active')){
+    document.querySelector('.me4').classList.remove('active');
+  };
+  if (document.querySelector('.me5').classList.contains('active')){
+    document.querySelector('.me5').classList.remove('active');
+  };
+
+  
+  if (document.querySelector('.me2').classList.contains('remove')){
+    document.querySelector('.me2').classList.remove('remove');
+    
+  };
+
+  document.querySelector('.me2').classList.add('active');
+  
+  
+}); 
+
+
+
+document.querySelector('.corInfo').addEventListener('click', () => {
+  if (document.querySelector('.me1').classList.contains('active')){
+    document.querySelector('.me1').classList.add('remove');
+  };
+  if (document.querySelector('.me2').classList.contains('active')){
+    document.querySelector('.me2').classList.add('remove');
+  };
+  if (document.querySelector('.me4').classList.contains('active')){
+    document.querySelector('.me4').classList.add('remove');
+  };
+  if (document.querySelector('.me5').classList.contains('active')){
+    document.querySelector('.me5').classList.add('remove');
+  };
+
+
+  if (document.querySelector('.me1').classList.contains('active')){
+    document.querySelector('.me1').classList.remove('active');
+  };
+  if (document.querySelector('.me2').classList.contains('active')){
+    document.querySelector('.me2').classList.remove('active');
+  };
+  if (document.querySelector('.me4').classList.contains('active')){
+    document.querySelector('.me4').classList.remove('active');
+  };
+  if (document.querySelector('.me5').classList.contains('active')){
+    document.querySelector('.me5').classList.remove('active');
+  };
+
+  
+  if (document.querySelector('.me3').classList.contains('remove')){
+    document.querySelector('.me3').classList.remove('remove');
+    
+  };
+
+  document.querySelector('.me3').classList.add('active');
+  
+  
+}); 
+
+
+
+
+document.querySelector('.pInfo').addEventListener('click', () => {
+  if (document.querySelector('.me1').classList.contains('active')){
+    document.querySelector('.me1').classList.add('remove');
+  };
+  if (document.querySelector('.me2').classList.contains('active')){
+    document.querySelector('.me2').classList.add('remove');
+  };
+  if (document.querySelector('.me3').classList.contains('active')){
+    document.querySelector('.me3').classList.add('remove');
+  };
+  if (document.querySelector('.me5').classList.contains('active')){
+    document.querySelector('.me5').classList.add('remove');
+  };
+
+
+  if (document.querySelector('.me1').classList.contains('active')){
+    document.querySelector('.me1').classList.remove('active');
+  };
+  if (document.querySelector('.me2').classList.contains('active')){
+    document.querySelector('.me2').classList.remove('active');
+  };
+  if (document.querySelector('.me3').classList.contains('active')){
+    document.querySelector('.me3').classList.remove('active');
+  };
+  if (document.querySelector('.me5').classList.contains('active')){
+    document.querySelector('.me5').classList.remove('active');
+  };
+
+  
+  if (document.querySelector('.me4').classList.contains('remove')){
+    document.querySelector('.me4').classList.remove('remove');
+    
+  };
+
+  document.querySelector('.me4').classList.add('active');
+  
+  
+}); 
+
+
+document.querySelector('.cInfo').addEventListener('click', () => {
+  if (document.querySelector('.me1').classList.contains('active')){
+    document.querySelector('.me1').classList.add('remove');
+  };
+  if (document.querySelector('.me2').classList.contains('active')){
+    document.querySelector('.me2').classList.add('remove');
+  };
+  if (document.querySelector('.me3').classList.contains('active')){
+    document.querySelector('.me3').classList.add('remove');
+  };
+  if (document.querySelector('.me4').classList.contains('active')){
+    document.querySelector('.me4').classList.add('remove');
+  };
+
+
+  if (document.querySelector('.me1').classList.contains('active')){
+    document.querySelector('.me1').classList.remove('active');
+  };
+  if (document.querySelector('.me2').classList.contains('active')){
+    document.querySelector('.me2').classList.remove('active');
+  };
+  if (document.querySelector('.me3').classList.contains('active')){
+    document.querySelector('.me3').classList.remove('active');
+  };
+  if (document.querySelector('.me4').classList.contains('active')){
+    document.querySelector('.me4').classList.remove('active');
+  };
+
+  
+  if (document.querySelector('.me5').classList.contains('remove')){
+    document.querySelector('.me5').classList.remove('remove');
+    
+  };
+
+  document.querySelector('.me5').classList.add('active');
+  
+  
+}); 
